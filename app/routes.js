67c79f7e6e4ab4e7a50da01a1ebae8586b3bc893
Mateus Brandao <mateusfbi@gmail.com>
2020-08-07 00:37:11 -0300

    Atualizações

--- app/routes.js
+++ app/routes.js
@@ -4,6 +4,9 @@ const CandidatoController = require("./controllers/CandidatoController");
 
 const routes = Router();
 
+routes.get("/",function(req,res){
+	res.sendFile(__dirname + "/html/index.html");
+});
 routes.post("/totvs/v1/candidato", CandidatoController.CadastroCandidato);
 routes.post("/totvs/v1/atualizar-cadastro", FuncionarioController.AtualizarCadastro);
 routes.post("/totvs/v1/atualizar-situacao", FuncionarioController.AlteraSituacao);
